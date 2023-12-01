a1fef85088b3e6c661f6c18ac4668778b0f2f22f
Carol <carolina.st@gmail.com>
2023-11-30 22:45:23 -0300

    Concluído alteração de status alugar e devolver

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
-    //no html o id identifica qual jogo e como existem 3, usamos a crase para concatenar e poder prencher com o ids diferentes.
-    //o botão está na tag "a" e o jogo que está alugado contém na tag div a informação de classe dashboard__item__img--rented.
-    //Essa classe é o que deixa o botão com cor opcaca e diferencia.
-    let imagem = gameClicado.querySelector('.dashboard__items__item');
-    //para pegar a div e a tag "a" do gameClicado 
-}
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+
+ }
